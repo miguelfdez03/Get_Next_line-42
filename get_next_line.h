fde8470e8afccc5c71b8d928d4075f5cfa981122
Miguel Angel Fernandez Munoz <miguel-f@c3r8s1.42malaga.com>
2025-02-04 14:09:45 +0100

    first steps

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miguel-f <miguel-f@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 20:43:03 by miguel-f          #+#    #+#             */
+/*   Updated: 2025/02/03 21:10:51 by miguel-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+
+#  define BUFFER_SIZE 42
+
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
