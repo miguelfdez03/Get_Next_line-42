0a576dc4a48a299965b18973de4a8a5af223b858
Miguel Angel Fernandez Munoz <miguel-f@c3r8s1.42malaga.com>
2025-02-04 14:27:48 +0100

    first steps

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: miguel-f <miguel-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 20:42:28 by miguel-f          #+#    #+#             */
-/*   Updated: 2025/02/03 21:09:57 by miguel-f         ###   ########.fr       */
+/*   Updated: 2025/02/04 14:23:33 by miguel-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 char	*get_next_line(int fd)
 {
-	
+	static char	str[BUFFER_SIZE];
+	int			bytes_read;
+	static int	counter = 0;
+
+	bytes_read = read(fd, str + counter, BUFFER_SIZE - counter - 1);
+	if (bytes_read <= 0)
+	{
+		return (NULL);
+	}
+	counter += bytes_read;
+	str[counter] = '\0';
+	return (str);
 }
