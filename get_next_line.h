8a59a0d830b7cb1b16555bd1f8d31c065b237857
Miguel Angel Fernandez Munoz <miguel-f@c3r4s4.42malaga.com>
2025-02-11 13:40:21 +0100

    everything okey

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: miguel-f <miguel-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 20:43:03 by miguel-f          #+#    #+#             */
-/*   Updated: 2025/02/03 21:10:51 by miguel-f         ###   ########.fr       */
+/*   Updated: 2025/02/10 18:01:36 by miguel-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,15 @@
 # include <unistd.h>
 
 # ifndef BUFFER_SIZE
-
 #  define BUFFER_SIZE 42
-
 # endif
 
 char	*get_next_line(int fd);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlen(const char *s);
 
 #endif
